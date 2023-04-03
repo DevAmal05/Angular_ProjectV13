d9c9fd07f8ffc82211118d089761a9ae15718085
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2023-04-03 15:59:19 +0200

    init

---
+++ angular/src/app/user.ts
@@ -0,0 +1,19 @@
+import { first } from "rxjs";
+
+export class User {
+    $key : string ;
+    id: string;
+
+    
+    Cin: string;
+   firstname: string;
+   lastname: string;
+   phone: string;
+
+   flamme:string;
+   temp:string;
+   gaz:string
+ 
+ 
+  
+}
